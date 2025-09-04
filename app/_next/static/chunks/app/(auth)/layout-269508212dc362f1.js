26a6edcb7ace9e10729697718cc8a2ba995d4b5e
gbenmoese <ese.gbenmo@uricreative.com>
2025-09-04 02:18:45 +0100

    login implemented

---
+++ app/_next/static/chunks/app/(auth)/layout-269508212dc362f1.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6695],{72057:function(e,t,r){Promise.resolve().then(r.bind(r,63265))},63265:function(e,t,r){"use strict";r.r(t);var n=r(57437),o=r(24033),s=r(2265);t.default=e=>{let{children:t}=e,r=(0,o.useRouter)(),u=(0,o.usePathname)(),f=u.startsWith("/subscribe/")&&3===u.split("/").length;return(0,s.useEffect)(()=>{if(f)return},[r]),(0,s.useEffect)(()=>{(null==u?void 0:u.includes("payment"))||localStorage.removeItem("beginToCheckout")},[u]),(0,n.jsx)(n.Fragment,{children:t})}},30622:function(e,t,r){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=r(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,f=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function q(e,t,r){var n,s={},c=null,l=null;for(n in void 0!==r&&(c=""+r),void 0!==t.key&&(c=""+t.key),void 0!==t.ref&&(l=t.ref),t)u.call(t,n)&&!i.hasOwnProperty(n)&&(s[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===s[n]&&(s[n]=t[n]);return{$$typeof:o,type:e,key:c,ref:l,props:s,_owner:f.current}}t.Fragment=s,t.jsx=q,t.jsxs=q},57437:function(e,t,r){"use strict";e.exports=r(30622)},24033:function(e,t,r){e.exports=r(50094)}},function(e){e.O(0,[2971,2472,1744],function(){return e(e.s=72057)}),_N_E=e.O()}]);
